e967454ba6b0f156a4b567d614733ccd257e2ba7
James Etchells <jaw.etch@gmail.com>
2022-01-24 12:11:51 -0800

    Add status div to display result of game

--- script.js
+++ script.js
@@ -23,34 +23,45 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computer) {
         console.log("tie");
+        document.getElementById('status').innerHTML = "Tie!";
     } else if (playerSelection == 'rock') {
         if (computer == 'scissors') {
-            console.log("I wiN")
-            playerScore++
+            console.log("I wiN");
+            playerScore++;
+            document.getElementById('status').innerHTML = "Win!";
         } else if (computer == 'paper') {
-            console.log("I lose")
-            computerScore++
+            console.log("I lose");
+            computerScore++;
+            document.getElementById('status').innerHTML = "Loss";
         }
     } else if (playerSelection == 'paper') {
         if (computer == 'rock') {
-            console.log("I Win")
-            playerScore++
+            console.log("I Win");
+            playerScore++;
+            document.getElementById('status').innerHTML = "Win!";
         } else if (computer == 'scissors') {
-            console.log("I lose")
-            computerScore++
+            console.log("I lose");
+            computerScore++;
+            document.getElementById('status').innerHTML = "Loss";
         }
     } else if (playerSelection == 'scissors') {
         if (computer == 'paper') {
-            console.log("I win")
-            playerScore++
+            console.log("I win");
+            playerScore++;
+            document.getElementById('status').innerHTML = "Win!";
         } else if (computer == 'rock') {
-            console.log("I lose")
-            computerScore++
+            console.log("I lose");
+            computerScore++;
+            document.getElementById('status').innerHTML = "Loss";
         }
     }
 
+    // Updates the scores in the Html
+
     document.getElementById('player').innerHTML = playerScore;
     document.getElementById('computer').innerHTML = computerScore;
+
+    // Declares a winner at 5 wins
 
     if (playerScore == 5) {
         alert('You Win!');
@@ -60,12 +71,6 @@ function playRound(playerSelection, computerSelection) {
         document.location.reload();
     }
 
-}
-
-// Initiates the game
-
-function game() {
-    playRound(playerSelection, computerSelection);
 }
 
 document.getElementById("button-1").addEventListener("click", function () {
