1a2f65c8ef0d3d596a822b886c14539be0144763
James Etchells <jaw.etch@gmail.com>
2022-01-24 13:49:08 -0800

    Add simple styles

--- script.js
+++ script.js
@@ -20,55 +20,64 @@ function playRound(playerSelection, computerSelection) {
     let computer = computerPlay();
     console.log(computer);
 
-
     if (playerSelection === computer) {
         console.log("tie");
-        document.getElementById('status').innerHTML = "Tie!";
+        document.getElementById('status').innerHTML = "TIE!";
     } else if (playerSelection == 'rock') {
         if (computer == 'scissors') {
             console.log("I wiN");
             playerScore++;
-            document.getElementById('status').innerHTML = "Win!";
+            document.getElementById('status').innerHTML = "WIN!";
         } else if (computer == 'paper') {
             console.log("I lose");
             computerScore++;
-            document.getElementById('status').innerHTML = "Loss";
+            document.getElementById('status').innerHTML = "LOSS";
         }
     } else if (playerSelection == 'paper') {
         if (computer == 'rock') {
             console.log("I Win");
             playerScore++;
-            document.getElementById('status').innerHTML = "Win!";
+            document.getElementById('status').innerHTML = "WIN!";
         } else if (computer == 'scissors') {
             console.log("I lose");
             computerScore++;
-            document.getElementById('status').innerHTML = "Loss";
+            document.getElementById('status').innerHTML = "LOSS";
         }
     } else if (playerSelection == 'scissors') {
         if (computer == 'paper') {
-            console.log("I win");
+            console.log("I WIN");
             playerScore++;
-            document.getElementById('status').innerHTML = "Win!";
+            document.getElementById('status').innerHTML = "WIN!";
         } else if (computer == 'rock') {
             console.log("I lose");
             computerScore++;
-            document.getElementById('status').innerHTML = "Loss";
+            document.getElementById('status').innerHTML = "LOSS";
         }
     }
 
-    // Updates the scores in the Html
 
-    document.getElementById('player').innerHTML = playerScore;
-    document.getElementById('computer').innerHTML = computerScore;
+    // Updates the scores in the Html if the score is less than or equal to 5
+
+    if (playerScore <= 5 && computerScore <= 5) {
+        document.getElementById('player').innerHTML = playerScore;
+        document.getElementById('computer').innerHTML = computerScore;
+    }
 
     // Declares a winner at 5 wins
 
     if (playerScore == 5) {
-        alert('You Win!');
-        document.location.reload();
+        document.getElementById('status').innerHTML = "YOU WIN!";
+        setTimeout(() => {
+            alert('Play again?');
+            document.location.reload();
+        }, 0100);
+
     } else if (computerScore == 5) {
-        alert('Computer Wins!')
-        document.location.reload();
+        document.getElementById('status').innerHTML = "MACHINE WINS!";
+        setTimeout(() => {
+            alert('Play again?');
+            document.location.reload();
+        }, 0100);
     }
 
 }
